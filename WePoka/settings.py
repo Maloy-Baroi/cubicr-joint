14d18a7d89b486723ca6aec43e53e99330d321f1
Maloy Baroi (XPL) <44752184+Maloy-Baroi@users.noreply.github.com>
2022-08-18 08:48:59 +0600

    Update settings.py

--- WePoka/settings.py
+++ WePoka/settings.py
@@ -124,11 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+# STATICFILES_DIRS =['/root/templates/colorlib-regform-6']
+STATIC_ROOT = '/root/cubicr/static'
+MEDIA_ROOT = '/root/cubicr/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
